00fc637779c946c57aebfe9d291baf9480f30e68
Winyata Senjaya <w.senjaya@gmail.com>
2023-11-25 22:08:28 +0700

    1. setup project

---
+++ src/app/about/profile/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./navbar";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <Navbar></Navbar>
+        </div>
+    )
+}
