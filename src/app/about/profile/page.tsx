7ee5936401299236b578d79ae56dc30bad1bb9f7
Winyata Senjaya <w.senjaya@gmail.com>
2023-11-25 22:45:15 +0700

    2. layout and template

--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from "./navbar";
 
 export default function AboutPage() {
     return (
         <div>
             <h1>Profile Page</h1>
-            <Navbar></Navbar>
         </div>
     )
 }
